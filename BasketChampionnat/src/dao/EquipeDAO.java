d2310eddb18cac20ac9c08cb57700c78d3b5c0dd
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-20 19:12:17 +0200

    Update EquipeDAO.java

--- BasketChampionnat/src/dao/EquipeDAO.java
+++ BasketChampionnat/src/dao/EquipeDAO.java
@@ -1,37 +1,70 @@
 package dao;
 
 import model.Equipe;
-import java.util.ArrayList;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
 import java.util.List;
 
-public class EquipeDAO implements IDAO<Equipe> {
-    private static List<Equipe> equipes = new ArrayList<>();
+public class EquipeDAO {
 
-    public void ajouter(Equipe e) {
-        equipes.add(e);
-    }
-
-    public void modifier(Equipe e) {
-        for (int i = 0; i < equipes.size(); i++) {
-            if (equipes.get(i).getId() == e.getId()) {
-                equipes.set(i, e);
-                break;
-            }
+    public void save(Equipe equipe) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(equipe);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw e;
         }
     }
 
-    public void supprimer(int id) {
-        equipes.removeIf(e -> e.getId() == id);
-    }
-
-    public Equipe getById(int id) {
-        for (Equipe e : equipes) {
-            if (e.getId() == id) return e;
+    public void update(Equipe equipe) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(equipe);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw e;
         }
-        return null;
     }
 
-    public List<Equipe> getAll() {
-        return equipes;
+    public void delete(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Equipe equipe = session.get(Equipe.class, id);
+            if (equipe != null) session.remove(equipe);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw e;
+        }
+    }
+
+    public Equipe findById(int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Equipe.class, id);
+        }
+    }
+
+    public List<Equipe> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Equipe> query = session.createQuery("from Equipe", Equipe.class);
+            return query.list();
+        }
+    }
+
+    public Equipe findByName(String nom) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Equipe> query = session.createQuery("from Equipe where nom = :nom", Equipe.class);
+            query.setParameter("nom", nom);
+            return query.uniqueResult();
+        }
     }
 }
+
