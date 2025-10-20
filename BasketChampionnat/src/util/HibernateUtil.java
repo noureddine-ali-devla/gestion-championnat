43800ff8e3f46febd60485972a1a9faa543ff3cd
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-20 19:43:33 +0200

    Update HibernateUtil.java

--- BasketChampionnat/src/util/HibernateUtil.java
+++ BasketChampionnat/src/util/HibernateUtil.java
@@ -1,13 +1,14 @@
 package util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 import model.Equipe;
 import model.Joueur;
 import model.Match;
 import model.Statistique;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+import model.User;
 
 public class HibernateUtil {
 
@@ -16,30 +17,21 @@ public class HibernateUtil {
     static {
         try {
             Configuration configuration = new Configuration();
-
-            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-            configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/basket_championnat?createDatabaseIfNotExist=true&useSSL=false&serverTimezone=UTC");
-            configuration.setProperty("hibernate.connection.username", "root");
-            configuration.setProperty("hibernate.connection.password", "");
-            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
-            configuration.setProperty("hibernate.show_sql", "true");
-            configuration.setProperty("hibernate.format_sql", "true");
-            configuration.setProperty("hibernate.current_session_context_class", "thread");
+            configuration.configure("hibernate.cfg.xml");
 
             configuration.addAnnotatedClass(Equipe.class);
             configuration.addAnnotatedClass(Joueur.class);
             configuration.addAnnotatedClass(Match.class);
             configuration.addAnnotatedClass(Statistique.class);
+            configuration.addAnnotatedClass(User.class);
 
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                     .applySettings(configuration.getProperties()).build();
 
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 
-        } catch (Exception e) {
-            System.err.println("Erreur lors de l'initialisation d'Hibernate : " + e.getMessage());
-            e.printStackTrace();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed: " + ex);
         }
     }
 
@@ -53,3 +45,4 @@ public class HibernateUtil {
         }
     }
 }
+
