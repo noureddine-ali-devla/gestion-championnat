4b3ea617df16e0077a86d8e4e95aad07796b1006
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-26 20:25:11 +0100

    Update JoueurDAO.java

--- BasketChampionnat/src/dao/JoueurDAO.java
+++ BasketChampionnat/src/dao/JoueurDAO.java
@@ -1,69 +1,50 @@
 package dao;
 
+import model.Equipe;
 import model.Joueur;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.query.Query;
-import util.HibernateUtil;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JoueurDAO {
 
-    public void save(Joueur joueur) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.persist(joueur);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) transaction.rollback();
-            throw e;
+    // Hardcoded list of players
+    private static final List<Joueur> JOUEURS = new ArrayList<>();
+
+    static {
+        Equipe e1 = new Equipe("Lions", "Casablanca", "lions@example.com");
+        Equipe e2 = new Equipe("Tigers", "Rabat", "tigers@example.com");
+
+        JOUEURS.add(new Joueur(1, "Amine", "Bennani", "Pivot", e1));
+        JOUEURS.add(new Joueur(2, "Youssef", "Karim", "Arrière", e2));
+        JOUEURS.add(new Joueur(3, "Rachid", "Ziani", "Ailier", e1));
+    }
+
+    public List<Joueur> listJoueurs() {
+        return JOUEURS;
+    }
+
+    public Joueur getById(int id) {
+        for (Joueur j : JOUEURS) {
+            if (j.getId() == id) return j;
+        }
+        return null;
+    }
+
+    public void addJoueur(Joueur joueur) {
+        JOUEURS.add(joueur);
+    }
+
+    public void updateJoueur(Joueur joueur) {
+        for (int i = 0; i < JOUEURS.size(); i++) {
+            if (JOUEURS.get(i).getId() == joueur.getId()) {
+                JOUEURS.set(i, joueur);
+                break;
+            }
         }
     }
 
-    public void update(Joueur joueur) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.merge(joueur);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) transaction.rollback();
-            throw e;
-        }
-    }
-
-    public void delete(int id) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            Joueur joueur = session.get(Joueur.class, id);
-            if (joueur != null) session.remove(joueur);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) transaction.rollback();
-            throw e;
-        }
-    }
-
-    public Joueur findById(int id) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.get(Joueur.class, id);
-        }
-    }
-
-    public List<Joueur> findAll() {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<Joueur> query = session.createQuery("from Joueur", Joueur.class);
-            return query.list();
-        }
-    }
-
-    public List<Joueur> findByEquipeId(int equipeId) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<Joueur> query = session.createQuery("from Joueur where equipe.id = :equipeId", Joueur.class);
-            query.setParameter("equipeId", equipeId);
-            return query.list();
-        }
+    public void deleteJoueur(int id) {
+        JOUEURS.removeIf(j -> j.getId() == id);
     }
 }
+
