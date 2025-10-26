4f2512829b6411a9b6d0f5199a272ab0d2673374
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-26 20:03:05 +0100

    Update Equipe.java

--- BasketChampionnat/src/model/Equipe.java
+++ BasketChampionnat/src/model/Equipe.java
@@ -1,35 +1,10 @@
 package model;
 
-import jakarta.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name = "equipes")
 public class Equipe {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
-    @Column(nullable = false, unique = true)
     private String nom;
-
-    @Column(nullable = false)
     private String ville;
-
-    @Column(nullable = false)
     private String email;
-
-    @OneToMany(mappedBy = "equipe", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Joueur> joueurs;
-
-    @OneToMany(mappedBy = "equipeDomicile", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Match> matchsDomicile;
-
-    @OneToMany(mappedBy = "equipeExterieur", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Match> matchsExterieur;
-
-    @OneToOne(mappedBy = "equipe", cascade = CascadeType.ALL)
-    private Statistique statistique;
 
     public Equipe() {}
 
@@ -39,64 +14,18 @@ public class Equipe {
         this.email = email;
     }
 
-    public int getId() {
-        return id;
-    }
+    // Getters et Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public String getNom() {
-        return nom;
-    }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
+    public String getVille() { return ville; }
+    public void setVille(String ville) { this.ville = ville; }
 
-    public String getVille() {
-        return ville;
-    }
-
-    public void setVille(String ville) {
-        this.ville = ville;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Joueur> getJoueurs() {
-        return joueurs;
-    }
-
-    public void setJoueurs(List<Joueur> joueurs) {
-        this.joueurs = joueurs;
-    }
-
-    public List<Match> getMatchsDomicile() {
-        return matchsDomicile;
-    }
-
-    public void setMatchsDomicile(List<Match> matchsDomicile) {
-        this.matchsDomicile = matchsDomicile;
-    }
-
-    public List<Match> getMatchsExterieur() {
-        return matchsExterieur;
-    }
-
-    public void setMatchsExterieur(List<Match> matchsExterieur) {
-        this.matchsExterieur = matchsExterieur;
-    }
-
-    public Statistique getStatistique() {
-        return statistique;
-    }
-
-    public void setStatistique(Statistique statistique) {
-        this.statistique = statistique;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
+
 
