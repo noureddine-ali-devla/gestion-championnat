aa9aae4f0b9a839b959d89bb08d1cc9aa15d93fb
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-26 11:05:34 +0100

    Update DataInitializer.java

--- BasketChampionnat/src/util/DataInitializer.java
+++ BasketChampionnat/src/util/DataInitializer.java
@@ -6,8 +6,6 @@ import model.Match;
 import model.Statistique;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import util.HibernateUtil;
-
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -40,15 +38,20 @@ public class DataInitializer {
             for (Equipe e : equipes) session.persist(e);
             for (Joueur j : joueurs) session.persist(j);
 
-            Match m1 = new Match(e1, e2, LocalDate.now().minusDays(3), 80, 75);
-            Match m2 = new Match(e2, e3, LocalDate.now().minusDays(2), 68, 70);
-            Match m3 = new Match(e3, e1, LocalDate.now().minusDays(1), 82, 78);
+            Match m1 = new Match(e1, e2, LocalDate.now().minusDays(3), 80, 75, "Gymnase Agadir");
+            Match m2 = new Match(e2, e3, LocalDate.now().minusDays(2), 68, 70, "Gymnase Casablanca");
+            Match m3 = new Match(e3, e1, LocalDate.now().minusDays(1), 82, 78, "Gymnase Marrakech");
 
             List<Match> matchs = Arrays.asList(m1, m2, m3);
             for (Match m : matchs) session.persist(m);
 
             for (Equipe e : equipes) {
                 Statistique s = new Statistique(e);
+                s.setMatchsJoues(2);
+                s.setVictoires(e.equals(e3) ? 2 : e.equals(e1) ? 1 : 0);
+                s.setDefaites(e.equals(e3) ? 0 : e.equals(e1) ? 1 : 2);
+                s.setButsMarques((int) (Math.random() * 150 + 100));
+                s.setButsEncaisses((int) (Math.random() * 150 + 100));
                 session.persist(s);
             }
 
