c4b194424987e1739336ecc1f8c3c2d5c26eab1c
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-26 20:33:57 +0100

    Update JoueurDAO.java

--- BasketChampionnat/src/dao/JoueurDAO.java
+++ BasketChampionnat/src/dao/JoueurDAO.java
@@ -1,50 +1,44 @@
 package dao;
 
-import model.Equipe;
 import model.Joueur;
+import model.Equipe;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JoueurDAO {
-
-    // Hardcoded list of players
-    private static final List<Joueur> JOUEURS = new ArrayList<>();
+    private static final List<Joueur> joueurs = new ArrayList<>();
 
     static {
         Equipe e1 = new Equipe("Lions", "Casablanca", "lions@example.com");
         Equipe e2 = new Equipe("Tigers", "Rabat", "tigers@example.com");
-
-        JOUEURS.add(new Joueur(1, "Amine", "Bennani", "Pivot", e1));
-        JOUEURS.add(new Joueur(2, "Youssef", "Karim", "Arrière", e2));
-        JOUEURS.add(new Joueur(3, "Rachid", "Ziani", "Ailier", e1));
+        joueurs.add(new Joueur("Youssef", "Bennani", "Arrière", 1, 0, e1));
+        joueurs.add(new Joueur("Omar", "El Idrissi", "Ailier", 2, 0, e1));
+        joueurs.add(new Joueur("Adam", "Bouzekri", "Pivot", 3, 0, e2));
     }
 
-    public List<Joueur> listJoueurs() {
-        return JOUEURS;
-    }
+    public List<Joueur> findAll() { return joueurs; }
 
-    public Joueur getById(int id) {
-        for (Joueur j : JOUEURS) {
+    public Joueur findById(int id) {
+        for (Joueur j : joueurs) {
             if (j.getId() == id) return j;
         }
         return null;
     }
 
-    public void addJoueur(Joueur joueur) {
-        JOUEURS.add(joueur);
-    }
+    public void save(Joueur joueur) { joueurs.add(joueur); }
 
-    public void updateJoueur(Joueur joueur) {
-        for (int i = 0; i < JOUEURS.size(); i++) {
-            if (JOUEURS.get(i).getId() == joueur.getId()) {
-                JOUEURS.set(i, joueur);
+    public void update(Joueur joueur) {
+        for (int i = 0; i < joueurs.size(); i++) {
+            if (joueurs.get(i).getId() == joueur.getId()) {
+                joueurs.set(i, joueur);
                 break;
             }
         }
     }
 
-    public void deleteJoueur(int id) {
-        JOUEURS.removeIf(j -> j.getId() == id);
+    public void delete(int id) {
+        joueurs.removeIf(j -> j.getId() == id);
     }
 }
+
 
