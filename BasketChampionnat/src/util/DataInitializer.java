a5bd2d511298a517f69c70c2044026d8a7898245
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 13:02:29 +0100

    Update DataInitializer.java

--- BasketChampionnat/src/util/DataInitializer.java
+++ BasketChampionnat/src/util/DataInitializer.java
@@ -23,18 +23,8 @@ public class DataInitializer {
         List<Equipe> equipes = Arrays.asList(e1, e2, e3);
         equipes.forEach(session::save);
 
-        Joueur j1 = new Joueur("Youssef", "Bennani", "Arrière", e1);
-        Joueur j2 = new Joueur("Omar", "El Idrissi", "Ailier", e1);
-        Joueur j3 = new Joueur("Adam", "Bouzekri", "Pivot", e2);
-        Joueur j4 = new Joueur("Karim", "Moutaouakil", "Meneur", e2);
-        Joueur j5 = new Joueur("Walid", "Tazi", "Arrière", e3);
-
         List<Joueur> joueurs = Arrays.asList(j1, j2, j3, j4, j5);
         joueurs.forEach(session::save);
-
-        Match m1 = new Match(e1, e2, LocalDate.now().minusDays(3), 80, 75);
-        Match m2 = new Match(e2, e3, LocalDate.now().minusDays(2), 68, 70);
-        Match m3 = new Match(e3, e1, LocalDate.now().minusDays(1), 82, 78);
 
         List<Match> matchs = Arrays.asList(m1, m2, m3);
         matchs.forEach(session::save);
