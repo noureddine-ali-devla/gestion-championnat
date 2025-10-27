2189671365cf9d729c34ed9aa2701a83b1df2a04
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 11:23:09 +0100

    Update MatchDAO.java

--- BasketChampionnat/src/dao/MatchDAO.java
+++ BasketChampionnat/src/dao/MatchDAO.java
@@ -1,2 +1,45 @@
+package dao;
+
+import model.Match;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
+import java.util.List;
+
+public class MatchDAO implements IDAO<Match> {
+
+    @Override
+    public void save(Match match) {
+        Database.save(match);
+    }
+
+    @Override
+    public void update(Match match) {
+        Database.update(match);
+    }
+
+    @Override
+    public void delete(int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Match match = session.get(Match.class, id);
+            if (match != null) Database.delete(match);
+        }
+    }
+
+    @Override
+    public Match findById(int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Match.class, id);
+        }
+    }
+
+    @Override
+    public List<Match> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Match> query = session.createQuery("from Match", Match.class);
+            return query.list();
+        }
+    }
+}
 
 
