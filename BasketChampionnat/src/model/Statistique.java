8ddde1b9c52fa064cc7f495453c17ef1d6b8bfc9
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 10:08:45 +0100

    Update Statistique.java

--- BasketChampionnat/src/model/Statistique.java
+++ BasketChampionnat/src/model/Statistique.java
@@ -1,49 +1,77 @@
 package model;
 
 public class Statistique {
+    private int id;
     private Equipe equipe;
     private int matchsJoues;
     private int victoires;
     private int defaites;
-    private int butsMarques;
-    private int butsEncaisses;
     private int points;
 
+    // Constructeur par défaut
     public Statistique() {}
 
+    // Constructeur avec équipe
     public Statistique(Equipe equipe) {
+        this.equipe = equipe;
+        this.matchsJoues = 0;
+        this.victoires = 0;
+        this.defaites = 0;
+        this.points = 0;
+    }
+
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Equipe getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(Equipe equipe) {
         this.equipe = equipe;
     }
 
-    public Equipe getEquipe() { return equipe; }
-    public void setEquipe(Equipe equipe) { this.equipe = equipe; }
-
-    public int getMatchsJoues() { return matchsJoues; }
-    public void setMatchsJoues(int matchsJoues) { this.matchsJoues = matchsJoues; }
-
-    public int getVictoires() { return victoires; }
-    public void setVictoires(int victoires) { this.victoires = victoires; }
-
-    public int getDefaites() { return defaites; }
-    public void setDefaites(int defaites) { this.defaites = defaites; }
-
-    public int getButsMarques() { return butsMarques; }
-    public void setButsMarques(int butsMarques) { this.butsMarques = butsMarques; }
-
-    public int getButsEncaisses() { return butsEncaisses; }
-    public void setButsEncaisses(int butsEncaisses) { this.butsEncaisses = butsEncaisses; }
-
-    public int getPoints() { return points; }
-    public void setPoints(int points) { this.points = points; }
-
-    public int getDifferenceButs() {
-        return butsMarques - butsEncaisses;
+    public int getMatchsJoues() {
+        return matchsJoues;
     }
 
-    public void enregistrerMatch(int marque, int encaisse, boolean victoire) {
+    public void setMatchsJoues(int matchsJoues) {
+        this.matchsJoues = matchsJoues;
+    }
+
+    public int getVictoires() {
+        return victoires;
+    }
+
+    public void setVictoires(int victoires) {
+        this.victoires = victoires;
+    }
+
+    public int getDefaites() {
+        return defaites;
+    }
+
+    public void setDefaites(int defaites) {
+        this.defaites = defaites;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    // Méthode utilitaire : mise à jour automatique des statistiques
+    public void enregistrerResultat(boolean victoire) {
         matchsJoues++;
-        butsMarques += marque;
-        butsEncaisses += encaisse;
         if (victoire) {
             victoires++;
             points += 2;
