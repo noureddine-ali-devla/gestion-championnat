86cf289d5b7954b7dc07f5ad5f847d905b3a944a
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 10:50:15 +0100

    Update UserDAO.java

--- BasketChampionnat/src/dao/UserDAO.java
+++ BasketChampionnat/src/dao/UserDAO.java
@@ -1,13 +1,1 @@
-package dao;
-
-public class UserDAO {
-
-    // Hardcoded user
-    private final String ADMIN_USERNAME = "admin";
-    private final String ADMIN_PASSWORD = "admin123";
-
-    public boolean authenticate(String username, String password) {
-        return ADMIN_USERNAME.equals(username) && ADMIN_PASSWORD.equals(password);
-    }
-}
 
