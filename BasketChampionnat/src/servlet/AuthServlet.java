f094ad6dd74986925f801c37819534a6082231ce
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 11:34:03 +0100

    Update AuthServlet.java

--- BasketChampionnat/src/servlet/AuthServlet.java
+++ BasketChampionnat/src/servlet/AuthServlet.java
@@ -1,3 +1,34 @@
+package servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import service.ChampionnatService;
+import model.User;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class AuthServlet extends HttpServlet {
+    private ChampionnatService service = new ChampionnatService();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User user = service.authentifier(username, password);
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            response.sendRedirect("dashboard.jsp");
+        } else {
+            response.sendRedirect("login.jsp?error=1");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+}
 
 
 
