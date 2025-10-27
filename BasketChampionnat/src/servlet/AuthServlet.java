bcf8dc6f262d1db9d6b71a0c2afc7880bf7955aa
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 10:52:55 +0100

    Update AuthServlet.java

--- BasketChampionnat/src/servlet/AuthServlet.java
+++ BasketChampionnat/src/servlet/AuthServlet.java
@@ -1,34 +1,4 @@
-package servlet;
 
-import dao.UserDAO;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
-import java.io.IOException;
-
-public class AuthServlet extends HttpServlet {
-
-    private final UserDAO userDAO = new UserDAO();
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        if (userDAO.authenticate(username, password)) {
-            HttpSession session = request.getSession();
-            session.setAttribute("user", username);
-            response.sendRedirect(request.getContextPath() + "/dashboard.jsp");
-        } else {
-            request.setAttribute("error", "Nom d’utilisateur ou mot de passe incorrect");
-            request.getRequestDispatcher("/pages/login.jsp").forward(request, response);
-        }
-    }
-}
 
 
 
