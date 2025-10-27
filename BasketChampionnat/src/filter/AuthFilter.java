a1474ed1a96785169ab089be19fe41b1fff54b6f
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 12:34:16 +0100

    Update AuthFilter.java

--- BasketChampionnat/src/filter/AuthFilter.java
+++ BasketChampionnat/src/filter/AuthFilter.java
@@ -1,1 +1,21 @@
+package filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+    public void init(FilterConfig filterConfig) {}
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendRedirect(req.getContextPath() + "/login.jsp");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+    public void destroy() {}
+}
 
