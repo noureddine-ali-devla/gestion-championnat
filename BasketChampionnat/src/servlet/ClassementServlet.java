6c50eae5f3c80e68987ef04be0d2186abbdba496
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 11:34:30 +0100

    Update ClassementServlet.java

--- BasketChampionnat/src/servlet/ClassementServlet.java
+++ BasketChampionnat/src/servlet/ClassementServlet.java
@@ -1,3 +1,25 @@
+package servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import service.ClassementService;
+import model.Equipe;
+import model.Statistique;
+
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet("/classement")
+public class ClassementServlet extends HttpServlet {
+    private ClassementService service = new ClassementService();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<Equipe, Statistique> classement = service.calculerClassement();
+        request.setAttribute("classement", classement);
+        request.getRequestDispatcher("pages/classement.jsp").forward(request, response);
+    }
+}
 
 
 
