4fa311577eb465dbcebf2782cf9a56ba170e0136
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 11:23:21 +0100

    Update UserDAO.java

--- BasketChampionnat/src/dao/UserDAO.java
+++ BasketChampionnat/src/dao/UserDAO.java
@@ -1,1 +1,44 @@
+package dao;
+
+import model.User;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
+import java.util.List;
+
+public class UserDAO implements IDAO<User> {
+
+    @Override
+    public void save(User user) {
+        Database.save(user);
+    }
+
+    @Override
+    public void update(User user) {
+        Database.update(user);
+    }
+
+    @Override
+    public void delete(int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            User user = session.get(User.class, id);
+            if (user != null) Database.delete(user);
+        }
+    }
+
+    @Override
+    public User findById(int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(User.class, id);
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> query = session.createQuery("from User", User.class);
+            return query.list();
+        }
+    }
+}
 
