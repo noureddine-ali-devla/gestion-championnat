78c33506957c417e85686a98ff602da9bfb776ed
noureddine-ali-devla <noureddineabarhane6@gmail.com>
2025-10-27 11:17:32 +0100

    Update IDAO.java

--- BasketChampionnat/src/dao/IDAO.java
+++ BasketChampionnat/src/dao/IDAO.java
@@ -1,1 +1,12 @@
+package dao;
+
+import java.util.List;
+
+public interface IDAO<T> {
+    void save(T entity);
+    void update(T entity);
+    void delete(int id);
+    T findById(int id);
+    List<T> findAll();
+}
 
